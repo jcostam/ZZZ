51e50a68f09fa26958405e8e34034b6848bb89db
Juanvi Costa <costa@ieselcaminas.org>
2021-02-16 20:18:54 +0100

    Commit 3 en rama Master

--- ZZZ/src/clase1.java
+++ ZZZ/src/clase1.java
@@ -3,7 +3,7 @@ public class clase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Clase original ");
+		System.out.println("Clase original cambios en la rama Master");
 	}
 
 }
