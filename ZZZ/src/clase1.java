0d2915cc62af0cfd0605dad52c899e157460c14d
Juanvi Costa <costa@ieselcaminas.org>
2021-02-16 20:17:30 +0100

    Commit 3 en rama refactorizar

--- ZZZ/src/clase1.java
+++ ZZZ/src/clase1.java
@@ -5,6 +5,7 @@ public class clase1 {
 		// TODO Auto-generated method stub
 		System.out.println("Clase original ");
 		System.out.println("Clase refactorizada ");
+		System.out.println("Clase documentada ");
 	}
 
 }
